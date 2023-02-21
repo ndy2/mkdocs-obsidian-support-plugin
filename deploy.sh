9444f01fda9b7620b054308b85ad127c11683d0e
ndy2 <emrdbs12@gmail.com>
2023-02-22 01:01:03 +0900

    update version

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,3 @@
 
 rm -rf dist
 python setup.py sdist && twine upload dist/* --skip-existing --config-file .pypirc
-pip install mkdocs-obsidian-support-plugin -U
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION_NUMBER = '0.3.5'
+VERSION_NUMBER = '0.4.0'
 
 
 def read_file(fname):
